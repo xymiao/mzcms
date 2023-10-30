41b8082d1b096ebdf4453d4d632339ea31575673
xymiao <xymiaotec@126.com>
2023-10-30 15:06:38 +0800

    更新版本调整

--- cms-web/src/main/java/com/xymiao/cms/web/conf/SecurityConfiguration.java
+++ cms-web/src/main/java/com/xymiao/cms/web/conf/SecurityConfiguration.java
@@ -1,6 +1,7 @@
-package com.xymiao.cms.web.util;
+package com.xymiao.cms.web.conf;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -12,22 +13,22 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
-public class SecurityConfiguration  {
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-				.authorizeHttpRequests((auth) -> auth
-						.requestMatchers("/*/**").permitAll()
-						.requestMatchers("/").permitAll()
-						.anyRequest().authenticated()
-				)
-				.httpBasic(withDefaults())
-				.formLogin(withDefaults());
-		return http.build();
-	}
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/*/**", "/").anonymous()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
+    }
 
 
-	// @formatter:off
+    // @formatter:off
 	@Bean
 	public UserDetailsService userDetailsService() {
 		String password = "{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG";
